48d1250c60b1e54ae4760ad04095a14dac331076
sc4tto <michele.scattolin1@gmail.com>
2026-08-14 01:39:50 +0200

    Add tracesheet/__init__.py

---
+++ tracesheet/__init__.py
@@ -0,0 +1,5 @@
+"""Raster-to-vector tracing tools used by Trace Sheet Studio."""
+
+from .engine import TraceSettings, TraceResult, trace_image
+
+__all__ = ["TraceSettings", "TraceResult", "trace_image"]
